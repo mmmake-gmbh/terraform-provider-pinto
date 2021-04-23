37184d400ade23262a2310eaf48605ec0730ac87
Andreas Zeißner <andreas.zeissner@whizus.com>
2021-04-23 13:49:20 +0200

    Setup provider test for resources and datasources

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.13
 
 require (
 	github.com/hashicorp/terraform-plugin-sdk/v2 v2.0.0-rc.2
+	github.com/stretchr/testify v1.3.0
 	gitlab.com/whizus/gopinto v0.0.0-20210409085238-87b8e356b8be
 	golang.org/x/oauth2 v0.0.0-20190604053449-0f29369cfe45
 )
---
+++ provider/provider_test.go
@@ -0,0 +1,38 @@
+package provider
+
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestProvider_HasNeededResources(t *testing.T) {
+	expectedResources := []string{
+		"pinto_dns_zone",
+		"pinto_dns_record",
+	}
+
+	resources := Provider().ResourcesMap
+	require.Equal(t, len(expectedResources), len(resources), "There are an unexpected number of registered resources")
+
+	for _, resource := range expectedResources {
+		require.Contains(t, resources, resource, "An expected resource was not registered")
+		require.NotNil(t, resources[resource], "A resource cannot have a nil schema")
+	}
+}
+
+func TestProvider_HasNeededDatasources(t *testing.T) {
+	expectedDatasources := []string{
+		"pinto_dns_zone",
+		"pinto_dns_zones",
+		"pinto_dns_record",
+		"pinto_dns_records",
+	}
+
+	datasources := Provider().DataSourcesMap
+	require.Equal(t, len(expectedDatasources), len(datasources), "There are an unexpected number of registered datasource")
+
+	for _, resource := range expectedDatasources {
+		require.Contains(t, datasources, resource, "An expected datasource was not registered")
+		require.NotNil(t, datasources[resource], "A datasource cannot have a nil schema")
+	}
+}
